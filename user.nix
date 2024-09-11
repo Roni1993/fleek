2c0d877420aeb80a38f9664ba37b3b64935539f0
Roman Weintraub <roman.weintraub@googlemail.com>
2024-09-11 19:45:58 +0000

    update host and user files

--- user.nix
+++ user.nix
@@ -1,6 +1,6 @@
 { config, pkgs, misc, ... }: {
   # FEEL FREE TO EDIT: This file is NOT managed by fleek. 
-  users.defaultUserShell = pkgs.nushell;
+#  users.defaultUserShell = pkgs.nushell;
   programs.starship.settings = {
   format = "[](#9A348E)$os$username[](bg:#DA627D fg:#9A348E)$directory[](fg:#DA627D bg:#FCA17D)$git_branch$git_status[](fg:#FCA17D bg:#86BBD8)$golang$gradle$java$nodejs$rust[](fg:#86BBD8 bg:#06969A)$docker_context[](fg:#06969A bg:#33658A)$time[ ](fg:#33658A)";
 
